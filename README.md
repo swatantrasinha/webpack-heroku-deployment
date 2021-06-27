d58e7f178df5762fb40c6b20f7326415e0ffd07a
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-06-27 14:31:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Source : https://www.freecodecamp.org/news/creating-a-production-ready-webpack-4-config-from-scratch/
+
+# Webpack Training 1
+
+This is an example repo to walk through a simple webpack config.
+
+## Quickstart Instructions
+
+Run `yarn install` to install necessary dependencies.
+
+
+/earlier code :
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "build": "webpack",
+    "start": "webpack",
+    "serve": "webpack serve"
+    */
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require('path')
+var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+module.exports = {
+    mode: 'production',
+    resolve: {
+        extensions: ['.js', '.css', '.scss']
+    },
+    entry: './src/index.js', // it can be multiple file. For multiple file use array with proper path
+    output: {// this is for output where you want to put your file after complete build process
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {// here we will load some external resource. which we are using to build our project
+        rules: [
+            {
+                test: /\.js$/, // it will find all js
+                use: 'babel-loader', // which will perform some functioanlity during build process.
+                exclude: [/node_modules/, /dist/]// expect this
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }]
+            }
+            // {
+            //     test: /\.js$/,
+            //     exclude: [/node_modules/, /dist/],
+            //     loader: 'eslint-loader'
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     use: [{
+            //         loader: 'style-loader'
+            //     }, {
+            //         loader: 'css-loader'
+            //     }, {
+            //         loader: 'sass-loader'
+            //     }]
+            // }
+        ]
+    },
+    plugins: [
+        // new CopyWebpackPlugin([{
+        //     from: './src/assets',
+        //     to: 'assets'
+        // }]),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            chunksSortMode: 'dependency'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 5000,
+    }
+}
